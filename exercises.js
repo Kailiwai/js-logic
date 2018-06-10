334bcf50e6c5978ffdad2aa167464729a88d2769
George Kailiwai <george.kailiwai.3@gmail.com>
2018-06-09 21:55:29 -1000

    code for #3

--- exercises.js
+++ exercises.js
@@ -67,7 +67,12 @@ console.log(login("test1234"));
  * The function will return true if the first number is greater than the second.
  * Console.log your result.
 */
-
+function isGreaterThan(first, second) {
+  if (first > second) {
+    return "true"
+  }
+}
+console.log(isGreaterThan(3, 2));
 
 
 /*
